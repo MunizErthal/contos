3668dea878ecabcc3c64065d139f8e94482d05e7
MunizErthal <32694606+MunizErthal@users.noreply.github.com>
2025-08-08 20:00:03 -0300

    Alterações para deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'your-app.firebaseapp.com',
-    projectId: 'your-app',
-    storageBucket: 'your-app.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+  firebase : {
+    apiKey: process.env['NG_APP_FIREBASE_API_KEY'] || '',
+    authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
+    databaseURL: process.env['NG_APP_FIREBASE_DATABASE_URL'] || '',
+    projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['NG_APP_FIREBASE_APP_ID'] || ''
   }
 };
